0297dcd2be0da6fb9e6d6a27c8a50caffa84e428
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-05-30 12:01:09 +0200

    sync

--- i3/.config/i3/start.sh
+++ i3/.config/i3/start.sh
@@ -1,9 +1,11 @@
 xrandr --output DP-1 --left-of HDMI-0
 xrandr --output DP-1 --primary
 
+nitrogen --restore &
+
 pkill picom
+pkill redshift-gtk
 picom &
 mpd &
 setxkbmap eu
-
-nitrogen --restore &
+redshift-gtk &
