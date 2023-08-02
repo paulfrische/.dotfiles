5e5056b3299ce0e79bfdbe7cfe8610ad6e4e03fb
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-08-02 16:46:12 +0200

    sync

--- nvim/.config/nvim/lua/paulfrische/plugins/lualine.lua
+++ nvim/.config/nvim/lua/paulfrische/plugins/lualine.lua
@@ -7,38 +7,6 @@ return {
       options = {
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
-      --   -- section_separators = { left = '', right = '' },
-      --   always_divide_middle = false,
-      --   globalstatus = true,
-      --   refresh = {
-      --     statusline = 50,
-      --     tabline = 50,
-      --     winbar = 50,
-      --   },
-      -- },
-      -- sections = {
-      --   lualine_a = { { 'mode', icons_enabled = true, icon = '' } },
-      --   lualine_b = {
-      --     'filetype',
-      --   },
-      --   lualine_c = {
-      --     { 'diagnostics', sources = { 'nvim_lsp' } },
-      --     function()
-      --       return '%='
-      --     end,
-      --     { 'filename', icons_enabled = true, icon = '' },
-      --   },
-      --   lualine_x = {
-      --   },
-      --   lualine_y = {},
-      --   lualine_z = {},
-      -- },
-      -- inactive_sections = {
-      --   lualine_a = {},
-      --   lualine_c = { 'filename' },
-      --   lualine_x = { 'location' },
-      --   lualine_y = {},
-      --   lualine_z = {},
       },
     })
   end,
--- nvim/.config/nvim/lua/paulfrische/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/paulfrische/plugins/treesitter.lua
@@ -1,29 +1,22 @@
+---@diagnostic disable: missing-fields
 return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
-    { 'nvim-treesitter/playground' },
     { 'mrjones2014/nvim-ts-rainbow' },
     { 'nvim-treesitter/nvim-treesitter-context' },
   },
+  build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = { 'lua', 'rust', 'python', 'html', 'css', 'comment' },
-      sync_install = true,
       auto_install = true,
 
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
       },
-
       indent = {
         enable = true,
       },
-
-      playground = {
-        enable = true,
-      },
-
       rainbow = {
         enable = false,
         extended_mode = true,
