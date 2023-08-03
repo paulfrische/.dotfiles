542d6e29a715d75ecaae861cfd7913e3fcd6a98b
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-08-03 12:47:29 +0200

    enable zen mode on UIEnter

--- nvim/.config/nvim/lua/paulfrische/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/paulfrische/plugins/zen-mode.lua
@@ -2,11 +2,14 @@ return {
   'folke/zen-mode.nvim',
   dependencies = { 'folke/twilight.nvim' },
   cmd = 'ZenMode',
+  event = 'UIEnter',
   config = function()
     require('zen-mode').setup({
       plugins = {
         twilight = { enabled = false },
       },
     })
+
+    vim.cmd('ZenMode')
   end,
 }
