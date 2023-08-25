1a110133beb4e289215366026307caf217062283
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-08-25 08:28:53 +0200

    add bufferline

---
+++ nvim/.config/nvim/lua/paulfrische/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/bufferline.nvim',
+  config = function ()
+    local bufferline = require('bufferline')
+    bufferline.setup({
+      options = {
+        separator_style = 'slant'
+      }
+    })
+  end
+}
