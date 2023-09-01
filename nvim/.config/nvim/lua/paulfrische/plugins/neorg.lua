d0abdb614eb660cd1bdd7e29714429f27c5dd08f
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-09-01 15:52:03 +0200

    sync

--- nvim/.config/nvim/lua/paulfrische/plugins/neorg.lua
+++ nvim/.config/nvim/lua/paulfrische/plugins/neorg.lua
@@ -1,17 +1,18 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  'nvim-neorg/neorg',
+  build = ':Neorg sync-parsers',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require("neorg").setup {
+    require('neorg').setup {
       load = {
-        ["core.defaults"] = {},    -- Loads default behaviour
-        ["core.concealer"] = {},   -- Adds pretty icons to your documents
-        ["core.dirman"] = {        -- Manages Neorg workspaces
+        ['core.defaults'] = {},    -- Loads default behaviour
+        ['core.concealer'] = {},   -- Adds pretty icons to your documents
+        ['core.dirman'] = {        -- Manages Neorg workspaces
           config = {
             workspaces = {
-              notes = "~/notes",
+              notes = '~/notes',
             },
+            default_workspace = 'notes',
           },
         },
       },
