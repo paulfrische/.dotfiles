ce138d4010175bd49244bc501ff151df10c0b895
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-09-17 15:07:32 +0200

    neogit is kinda cool

---
+++ nvim/.config/nvim/lua/paulfrische/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  'NeogitOrg/neogit',
+  cmd = {
+    'Neogit',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'nvim-telescope/telescope.nvim', -- optional
+    'sindrets/diffview.nvim', -- optional
+  },
+  config = true,
+}
