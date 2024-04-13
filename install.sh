b574f3c79e0af0341b51abfce760ab3940df753f
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2024-04-13 11:28:15 +0200

    add gtk config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ set -e
 stow -vt $HOME alacritty
 stow -vt $HOME kitty
 stow -vt $HOME i3
+stow -vt $HOME gtk
 stow -vt $HOME launchme
 stow -vt $HOME bash
 stow -vt $HOME scripts
--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,7 @@ set -e
 stow -vDt $HOME alacritty
 stow -vDt $HOME kitty
 stow -vDt $HOME i3
+stow -vDt $HOME gtk
 stow -vDt $HOME launchme
 stow -vDt $HOME bash
 stow -vDt $HOME polybar
