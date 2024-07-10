f917d798bb89fb828a0d91b1d1609e106fb3a82c
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2024-07-10 13:46:50 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ set -e
 
 
 stow -vt $HOME alacritty
-stow -vt $HOME kitty
-stow -vt $HOME i3
-stow -vt $HOME gtk
-stow -vt $HOME launchme
 stow -vt $HOME bash
-stow -vt $HOME nushell
-stow -vt $HOME scripts
+stow -vt $HOME i3
 stow -vt $HOME polybar
--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,6 @@
 set -e
 
 stow -vDt $HOME alacritty
-stow -vDt $HOME kitty
-stow -vDt $HOME i3
-stow -vDt $HOME gtk
-stow -vDt $HOME launchme
 stow -vDt $HOME bash
-stow -vDt $HOME nushell
+stow -vDt $HOME i3
 stow -vDt $HOME polybar
